86282b9b060193db29043eaa250321cef8bcd251
Jess Dworcan <jessdworcan@gmail.com>
2022-11-24 10:49:06 +0200

    Initial commit

---
+++ promptPage.js
@@ -0,0 +1,12 @@
+//Prompt page with prompt bar for user to input a prompt
+//Upload photos to feed into AI
+
+import React from 'react'
+
+
+
+export const promptPage = () => {
+    return (
+        <div>promptPage</div>
+    )
+}
