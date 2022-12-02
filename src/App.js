a073a4dea01e0638fedfd91e9d02e32b0c235ec5
Jess Dworcan <jessdworcan@gmail.com>
2022-12-02 15:17:05 +0200

    Buttom

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ const Title = styled.div`
   color: green;
   font-size: 50px;
   text-align: center;
-`
+`;
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  // hover colour change
+  &:hover {
+    color: red; // <Thing> when hovered
+  }
+`;
 
 function App() {
   return (
@@ -14,6 +31,9 @@ function App() {
       <Title>
         Welcome to the fancy AI prompt app thing
       </Title >
+      <Button>
+        Render Image
+      </Button>
     </div>
   );
 }
